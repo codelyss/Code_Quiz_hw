107c507a2bc52d46ea0a0ae2d20af87dc97e923e
codelyss <atcodeveloper@gmail.com>
2019-12-20 21:48:39 -0500

    Button to select quiz

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+var ddlquiz = document.getElementById('ddlQuiz');
+var lblquestion = document.getElementById('lblQuestion');
+var lblResult = document.getElementById('lblResult');
+var timerdisplay = document.getElementById('timerdisplay');
+
+var txtScore = document.getElementById('txtScore');
+var txtUserName = document.getElementById('txtUserName');
+var txtHighScores = document.getElementById('txtHighScores');
+
+var btn1 = document.getElementById('btnAnswer1');
+var btn2 = document.getElementById('btnAnswer2');
+var btn3 = document.getElementById('btnAnswer3');
+var btn4 = document.getElementById('btnAnswer4');
+
+function SelectQuiz(quizName){
+    //changes the dropdown value (text) to the selected quiz
+    ddlquiz.innerHTML = quizName;
+}
+
+
+
 // write out function
 SaveScore()
 ShowMainContainer()
